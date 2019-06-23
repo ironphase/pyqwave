d3caa547f621616ef0790f7f59942ea60c52eae7
Miguel A <ironphase@gmail.com>
2019-06-23 14:16:02 -0700

    Initial check in

---
+++ pyqwave/helpers.py
@@ -0,0 +1,37 @@
+######################################################################################################
+# PyQWave Toolkit
+# Authors: Daniel Sepulveda    Toronto ON (Canada)
+#          Miguel A Sepulveda  Sunnyvale CA (USA)
+#
+######################################################################################################
+import logging
+
+######################################################################################################
+def is_string_integer( string ):
+    try:
+        value = int(string)
+        return True
+    except ValueError:
+        pass
+
+    return False
+
+######################################################################################################
+def is_string_float( string ):
+    try:
+        value = float(string)
+        return True
+    except ValueError:
+        pass
+
+    return False
+
+######################################################################################################
+def is_integer( number ):
+    return isinstance(number,int)
+
+######################################################################################################
+def is_float( number ):
+    return isinstance(number, float)
+
+    
---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+def readme():
+  with open('README.rst') as f:
+    return f.read()
+
+setup(name='pyqwave',
+      version='0.1.0',
+      description='Toolkit for Quantum wave propagations',    
+      long_description=readme(),  
+      author='Daniel & Miguel A Sepulveda',
+      license='Unknown',
+      packages=['pyqwave'],
+      install_requires=['matplotlib', 
+                        'numpy',
+                        'Pillow',
+                        'pyquaternion',
+                        'uuid',
+                        'nose',
+                        'pyYAML',
+                        ],
+      include_package_data=True,
+      zip_safe=False,
+      test_suite='nose.collector',
+      tests_require=['nose'],
+)
